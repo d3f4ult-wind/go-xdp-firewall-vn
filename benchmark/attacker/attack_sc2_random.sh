bfa665434137b30d9c9e8025c1d0e99f28c6ceb4
d3f4ult-wind <lenamphong23062004@gmail.com>
2026-06-08 16:57:20 +0700

    u10000 to u100

--- benchmark/attacker/attack_sc2_random.sh
+++ benchmark/attacker/attack_sc2_random.sh
@@ -8,7 +8,7 @@
 # =============================================================
 
 VICTIM_IP="10.10.2.2"
-DEFAULT_PPS="u10000" # u10000 = wait 10,000 microseconds -> ~100 pps mỗi tiến trình (5 luồng = 500 pps + overhead = ~6k pps total)
+DEFAULT_PPS="u100" # wait 100 microseconds -> ~10k pps mỗi tiến trình (5 luồng = 50k pps + overhead = ~600k pps total)
 
 echo "[*] [ATTACKER] Đang chuẩn bị đạn dược Botnet (Random Source IP)..."
 
